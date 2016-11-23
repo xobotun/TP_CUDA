d19145713f1a1df297a4e16d2d0b20c2a6efdf93
Paul Maminov <xobotun@inbox.ru>
2016-11-23 14:22:11 +0300

    Now with 146% better coin recognition!
    And 146x times slower. :(

--- src.py
+++ src.py
@@ -2,17 +2,94 @@ import numpy;
 import cv2;
 import os;
 
-IMAGE_PATH_ROOT = "E:\YandexDisk\Learning\TechnoPark\CUDA\TP_CUDA\photos\\";
-IMAGE_PATH_DETECTOR_IN = "detector\\";
-IMAGE_PATH_DETECTOR_OUT = "detector\out\\";
+IMAGE_PATH_ROOT = "E:\YandexDisk\Learning\TechnoPark\CUDA\TP_CUDA\photos\\"
+IMAGE_PATH_DETECTOR_IN = "detector\\"
+IMAGE_PATH_DETECTOR_OUT = "detector\out\\"
+YELLOW_COIN_COLORS_BGR = [
+    # 10 kops
+    ([45, 65, 85], [45, 65, 85]),       #55412d
+    ([24, 32, 45], [24, 32, 45]),       #2d2018
+    ([30, 45, 59], [30, 45, 59]),       #3b2d1e
+    ([29, 65, 88], [29, 65, 88]),       #58411d
+    ([24, 32, 44], [24, 32, 44]),       #2c2018
+    ([29, 44, 72], [29, 44, 72]),       #482c1d
+    ([35, 58, 88], [35, 58, 88]),       #583a23
+    # 50 kops
+    ([32, 54, 72], [32, 54, 72]),       #483620
+    ([28, 56, 80], [28, 56, 80]),       #50381c
+    ([33, 48, 60], [33, 48, 60]),       #3c3021
+        # enlightened
+    ([96, 167, 208], [96, 167, 208]),   #d0a760
+    ([63, 121, 157], [63, 121, 157]),   #9d793f
+    # 10 roubles
+    ([28, 55, 74], [28, 55, 74]),       #4a371c
+]
+YELLOW_COIN_COLOR_BOUNDARY = ([-10, -10, -15], [15, 15, 20])
+SILVER_COIN_COLORS_BGR = [
+    # 5 roubles
+    ([69, 77, 86], [69, 77, 86]),       #564d45
+    ([68, 73, 76], [68, 73, 76]),       #4c4944
+    ([59, 60, 66], [59, 60, 66]),       #423c3b
+        # enlightened
+    ([124, 173, 202], [124, 173, 202]), #caad7c
+    ([181, 155, 134], [181, 155, 134]), #869bb5
+    #([173, 150, 130], [173, 150, 130]), #8296ad
+    #([141, 126, 112], [141, 126, 112]), #707e8d
+    # 1 rouble
+    ([55, 63, 74], [55, 63, 74]),       #4a3f37
+    # 2 roubles
+    ([59, 65, 72], [59, 65, 72]),       #48413b
+        # enlightened
+    ([136, 188, 210], [136, 188, 210]), #d2bc88
+    # 10 roubles
+    ([82, 85, 86], [82, 85, 86]),       #565552
+]
+SILVER_COIN_COLOR_BOUNDARY = ([-15, -15, -15], [15, 15, 15])
+
+def print_image(image):
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.imshow('image', image)
+    cv2.resizeWindow('image', 1024, 768)
+    cv2.waitKey(0)
+
+def apply_masks(image):
+    masked_image = numpy.zeros(image.shape, numpy.uint8)
+    for (lower, upper) in YELLOW_COIN_COLORS_BGR:
+        lower = list(map(sum, zip(lower, YELLOW_COIN_COLOR_BOUNDARY[0])))
+        lower = numpy.array(lower, dtype="uint8")
+        upper = list(map(sum, zip(upper, YELLOW_COIN_COLOR_BOUNDARY[1])))
+        upper = numpy.array(upper, dtype="uint8")
+
+        mask = cv2.inRange(image, lower, upper)
+        current_masked_image = cv2.bitwise_and(image, image, mask=mask)
+        masked_image = cv2.bitwise_or(masked_image, current_masked_image)
+
+    for (lower, upper) in SILVER_COIN_COLORS_BGR:
+        lower = list(map(sum, zip(lower, SILVER_COIN_COLOR_BOUNDARY[0])))
+        lower = numpy.array(lower, dtype="uint8")
+        upper = list(map(sum, zip(upper, SILVER_COIN_COLOR_BOUNDARY[1])))
+        upper = numpy.array(upper, dtype="uint8")
+
+        mask = cv2.inRange(image, lower, upper)
+        current_masked_image = cv2.bitwise_and(image, image, mask=mask)
+        masked_image = cv2.bitwise_or(masked_image, current_masked_image)
+
+    return masked_image
+"""
+    cv2.namedWindow('images', cv2.WINDOW_NORMAL)
+    cv2.imshow("images", numpy.hstack([image, masked_image]))
+    cv2.resizeWindow('images', 2048, 768)
+    cv2.waitKey(0)
+"""
 
 def preprocess_image(image):
-    smoother = numpy.ones((13, 13), numpy.float32) / (10*10);
-    smoothed = cv2.filter2D(image, -1, smoother);
-    blurred = cv2.GaussianBlur(smoothed, (15, 15), 0);
-    #bilatered = cv2.bilateralFilter(image, 45, 160, 200);  # http://docs.opencv.org/3.0-beta/modules/imgproc/doc/filtering.html?highlight=bilateral#cv2.bilateralFilter
-    thresholded = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 1.7);
-    return thresholded;
+    blurred = cv2.GaussianBlur(image, (15, 15), 0)
+    masked = apply_masks(blurred)
+    masked = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+    #thresholded = cv2.adaptiveThreshold(masked, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 1.7)
+    _,  thresholded = cv2.threshold(masked, 10, 255, cv2.THRESH_BINARY_INV )
+    blurred_again = cv2.GaussianBlur(thresholded, (37, 37), 0)
+    return blurred_again
 """
     cv2.namedWindow('original', cv2.WINDOW_NORMAL);
     cv2.namedWindow('smoothed', cv2.WINDOW_NORMAL);
@@ -32,25 +109,30 @@ def preprocess_image(image):
 """
 
 def get_circles(image):
-    circles = cv2.HoughCircles(image, cv2.cv.CV_HOUGH_GRADIENT, 1, 90, param1=100, param2=30, minRadius=0, maxRadius=150);
-    return numpy.uint16(numpy.around(circles));
+    circles = cv2.HoughCircles(image, cv2.cv.CV_HOUGH_GRADIENT, 2, 100, param1=100, param2=85, minRadius=25, maxRadius=150);
+    if circles != None:
+        circles = numpy.uint16(numpy.around(circles))
+    return circles
 
 
 path = IMAGE_PATH_ROOT+IMAGE_PATH_DETECTOR_IN;
 filenames = next(os.walk(path))[2];
 for image in filenames:
-    img_gray = cv2.imread(path+image, 0);
-    circles = get_circles(preprocess_image(img_gray));
+    img_gray = cv2.imread(path+image, -1);
+    filtered_image = preprocess_image(img_gray);
+
+    circles = get_circles(filtered_image)
 
     img = cv2.imread(path+image, -1);
-    for i in circles[0,:]:
-        cv2.circle(img,(i[0],i[1]),i[2],(40,150,40),5)
-        cv2.circle(img,(i[0],i[1]),2,(10,140,170),10)
+    if circles != None:
+        for i in circles[0,:]:
+            cv2.circle(img,(i[0],i[1]),i[2],(40,150,40),5)
+            cv2.circle(img,(i[0],i[1]),2,(10,140,170),10)
 
-    cv2.namedWindow('circles', cv2.WINDOW_NORMAL);
-    cv2.imshow('circles', img);
+    cv2.namedWindow(image, cv2.WINDOW_NORMAL);
+    cv2.imshow(image, img);
     cv2.imwrite(IMAGE_PATH_ROOT+IMAGE_PATH_DETECTOR_OUT+image, img)
-    cv2.resizeWindow('circles', 1024, 768);
+    cv2.resizeWindow(image, 1024, 768);
 
-    cv2.waitKey(0);
-    cv2.destroyAllWindows();
+cv2.waitKey(0);
+cv2.destroyAllWindows();
